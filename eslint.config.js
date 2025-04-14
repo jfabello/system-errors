ddaa409a9cd81e9b432b3232113205e19cb4bf6c
Juan F. Abello <juan@jfabello.com>
2025-04-14 18:07:06 -0500

    Fixed and enhanced eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,16 @@
-import js from '@eslint/js';
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
 
-export default [js.configs.recommended];
+const eslintConfig = {
+	files: ["**/*.js"],
+	plugins: { js },
+	extends: ["js/recommended"],
+	languageOptions: {
+		ecmaVersion: 2022,
+		sourceType: "module",
+		globals: globals.node
+	}
+};
+
+export default defineConfig([eslintConfig]);
